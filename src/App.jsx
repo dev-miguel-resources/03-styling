113114c0165815342d48d7f945c6d187e49307cb
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-06-14 20:55:01 -0400

    first commit: style-module.css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./global.css";
+import Car from "./components/Car/Car";
+import Greetings from "./components/Greetings/Greetings";
+
+function App() {
+  return (
+    <div>
+      <Car />
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Car/Car.jsx
@@ -0,0 +1,5 @@
+const Car = () => {
+  return <p className="box box2">I am the Car!</p>
+}
+
+export default Car
